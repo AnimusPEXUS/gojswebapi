9fb86e8f8f8428127719b18bcb3d3b2c4e0e05e8
Animus PEXUS <animuspexus@protonmail.com>
2021-03-28 23:33:46 +0300

    ic

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/AnimusPEXUS/gojswebapi
 go 1.15
 
 require (
-	github.com/AnimusPEXUS/gojstools v0.0.0-20210327184709-7679315fbbd9
+	github.com/AnimusPEXUS/gojstools v0.0.0-20210327193209-4c20672e96a8
 	github.com/AnimusPEXUS/utils v0.0.0-20210324003110-4bd50c7dd42e
 )
---
+++ window/Window.go
@@ -0,0 +1,42 @@
+package window
+
+import (
+	"syscall/js"
+
+	gojstoolsutils "github.com/AnimusPEXUS/gojstools/utils"
+	"github.com/AnimusPEXUS/gojswebapi/dom"
+)
+
+type Window struct {
+	JSValue *js.Value
+}
+
+func NewWindowFromGlobalThis() (*Window, error) {
+	return NewWindowFromJSValue(gojstoolsutils.JSValueLiteralToPointer(js.Global()))
+}
+
+func NewWindowFromJSValue(value *js.Value) (*Window, error) {
+	self := &Window{}
+	self.JSValue = value
+	return self, nil
+}
+
+func (self *Window) Open(url string, windowname string, windowfeatures *string) (*Window, error) {
+
+	windowfeatures_val := js.Undefined()
+	if windowfeatures != nil {
+		windowfeatures_val = js.ValueOf(*windowfeatures)
+	}
+
+	cres := self.JSValue.Call("open", url, windowname, windowfeatures_val)
+	ret := &Window{
+		JSValue: &cres,
+	}
+	return ret, nil
+}
+
+func (self *Window) GetDocument() *dom.Document {
+	doc := self.JSValue.Get("document")
+	ret := dom.NewDocumentFromJsValue(&doc)
+	return ret
+}
